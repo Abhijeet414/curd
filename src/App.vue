ba4b103ccefdd9c0b77588fd9023c0e849b5f70d
ABHIJEET MANDAL <abhijeet.mandal@serialxtech.com>
2021-06-11 16:51:54 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    
+    <curd/>
+  </div>
+</template>
+
+<script>
+import Curd from '../curd/src/components/curd.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+   // HelloWorld,
+    Curd
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
